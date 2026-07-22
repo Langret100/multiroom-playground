c2f58561888197e2ec19c52f85917f0fba97aa60
Langret100 <99009867+Langret100@users.noreply.github.com>
2026-07-22 15:36:36 +0900

    Add files via upload

--- js/core/net.js
+++ js/core/net.js
@@ -391,6 +391,6 @@ if (isDuel && humans.length === 1){
   }
 
   // Build marker for debugging deployments
-  window.__BUILD_ID = "2026-07-22-soccer-dribble-parity-v12";
+  window.__BUILD_ID = "2026-07-22-soccer-dribble-visual-v13";
   window.Net = { nowHHMM, makeClient, safeText, setStatus };
 })();
